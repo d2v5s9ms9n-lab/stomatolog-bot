1090372e833fe747434d85633d7a0437dd7ea4f1
d2v5s9ms9n-lab <d2v5s9ms9n@privaterelay.appleid.com>
2026-06-13 01:23:50 +0300

    Add main bot file

---
+++ bot.py
@@ -0,0 +1,54 @@
+import logging
+from telegram import Update
+from telegram.ext import Application, CommandHandler, CallbackQueryHandler
+from config import BOT_TOKEN, ADMIN_ID
+from database import Session, Appointment, Patient
+from keyboards import main_menu
+import handlers
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
+
+async def start(update: Update, context):
+    user = update.effective_user
+    with Session() as session:
+        patient = session.query(Patient).filter_by(telegram_id=user.id).first()
+        if not patient:
+            patient = Patient(telegram_id=user.id, first_name=user.first_name, last_name=user.last_name)
+            session.add(patient)
+            session.commit()
+    await update.message.reply_text(f'👋 Привет, {user.first_name}!\n\nЯ бот клиники стоматолога Али.\n\nЧем могу помочь?', reply_markup=main_menu())
+
+async def handle_callback(update: Update, context):
+    query = update.callback_query
+    await query.answer()
+    data = query.data
+    if data == 'book':
+        await handlers.show_services(update, context)
+    elif data == 'my_appointments':
+        await handlers.show_appointments(update, context)
+    elif data == 'info':
+        await handlers.show_info(update, context)
+    elif data == 'ton_payment':
+        await handlers.show_ton_payment(update, context)
+    elif data.startswith('service_'):
+        service_key = data.split('_')[1]
+        await handlers.select_service(update, context, service_key)
+    elif data == 'back_to_main':
+        await query.edit_message_text('Чем могу помочь?', reply_markup=main_menu())
+    elif data == 'confirm_appointment':
+        await handlers.confirm_appointment(update, context)
+    elif data == 'cancel_appointment':
+        await handlers.cancel_appointment(update, context)
+    elif data == 'pay_with_ton':
+        await handlers.pay_with_ton(update, context)
+
+def main():
+    app = Application.builder().token(BOT_TOKEN).build()
+    app.add_handler(CommandHandler('start', start))
+    app.add_handler(CallbackQueryHandler(handle_callback))
+    logger.info('Bot started')
+    app.run_polling()
+
+if __name__ == '__main__':
+    main()
